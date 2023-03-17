10373cd15617f4c61dc76ed50ab5d1aff5fa4030
Siddhi Agrawal <siddhi.agrawal@SIAGRA-BLRM20.local>
2023-03-17 23:52:31 +0530

    CSS Styling added

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,14 @@
 import { hasSelectionSupport } from '@testing-library/user-event/dist/utils';
-import React,{useState} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 
 export default function TodoForm(props) {
     const[input,setInput] = useState('');
+
+    const inputRef = useRef(null);
+
+    useEffect(()=>{
+        inputRef.current.focus();
+    })
 
     const handleChange = (e) => {
         setInput(e.target.value)
@@ -28,6 +34,7 @@ export default function TodoForm(props) {
             value={input}
             placeholder='Add a todo items'
             onChange={handleChange}
+            ref={inputRef}
             />
             <button className='todo-button'>Add Todo</button>
         </form>
