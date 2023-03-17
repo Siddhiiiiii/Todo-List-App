3fc100bdac4dee85ce2b6e0b3a48f2edeb54aed1
Siddhi Agrawal <siddhi.agrawal@SIAGRA-BLRM20.local>
2023-03-17 23:41:29 +0530

    Delete edit done

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,11 +4,23 @@ import {FaUserEdit} from 'react-icons/fa';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
-function Todo({todos,completeTodo,removeTodo,editTodo}) {
+function Todo({todos,completeTodo,removeTodo,updateTodo}) {
   const[edit,setEdit] = useState({
     id : null,
     value : ''
   });
+
+  const submitUpdate = value => {
+    updateTodo(edit.id,value);
+    setEdit({
+        id : null,
+        value : ''
+    });
+  };
+
+  if(edit.id){
+    return <TodoForm edit={edit} onSubmit={submitUpdate}/>
+  }
 
   return todos.map((todo,index) => (
     <div className={todo.isComplete ? 'todo-row complete' : 'todo-row' }
@@ -23,7 +35,7 @@ function Todo({todos,completeTodo,removeTodo,editTodo}) {
             onClick={()=>removeTodo(todo.id)}
             className='delete-icon'/>
             <FaUserEdit
-            onClick={()=>editTodo({id : todo.id,value : todo.text})}
+            onClick={()=>setEdit({id : todo.id,value : todo.text})}
             className='edit-icon'/>
         </div>
     </div>    
