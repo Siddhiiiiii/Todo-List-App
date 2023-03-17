fcf3c9b5e0dfdff86e2f003f22fc1ff2bd4cc160
Siddhi Agrawal <siddhi.agrawal@SIAGRA-BLRM20.local>
2023-03-17 23:07:55 +0530

    components added

---
+++ src/components/TodoForm.js
@@ -0,0 +1,36 @@
+import { hasSelectionSupport } from '@testing-library/user-event/dist/utils';
+import React,{useState} from 'react'
+
+export default function TodoForm(props) {
+    const[input,setInput] = useState('');
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id : Math.floor(Math.random()*10000),
+            text : input
+        });
+
+        setInput('');
+    }
+
+
+    return (
+        <form className='todo-form' onSubmit={handleSubmit}>
+            <input
+            type = 'text'
+            className='todo-input'
+            name='text'
+            value={input}
+            placeholder='Add a todo items'
+            onChange={handleChange}
+            />
+            <button className='todo-button'>Add Todo</button>
+        </form>
+    )
+}
+
